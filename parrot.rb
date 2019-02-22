09034818c46e34d17b9f57b50b03ccfd5e29dbde
Jared Stromberg <jaredstromberg@gmail.com>
2019-02-22 23:17:50 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(argument)
+  begin
+  empty = parrot()
+  if empty 
+    puts = "Squawk!"
+  end
+  rescue
+    return "Squawk!"
+  end
+  puts argument
+  return argument
+end
